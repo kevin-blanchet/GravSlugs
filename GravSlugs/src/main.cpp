f34b08930e14b6abf256fca9e05435848a9e8f0a
Hugo Carricart <hugo.carricart@quantor.fr>
2024-01-15 11:45:21 +0100

    New main.cpp (GravSlugs)

---
+++ GravSlugs/src/main.cpp
@@ -0,0 +1,14 @@
+/**
+* @file main.cpp
+* @brief 
+* @authors BLANCHET Kevin, CARRICART Hugo, PRADEL Hugo
+* @copyright (c) YNOV 2024
+* @date 15/01/2024
+* @version 0.0.1
+*/
+#include <GravEngine.h>
+
+int main()
+{
+	GravEngine::print_hello();
+}
